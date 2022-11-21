134e05459a15fe5da059adea63f8904732848fc7
çağrı dönmez <ertgrldnmz@outlook.com>
2022-11-21 13:37:56 +0300

    if komutları yazıldı ve test edildi.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time =DateTime.Now.Hour;
+
+        if(time>=6 && time<11)
+        {
+        Console.WriteLine("Günaydın");
+        }
+        else if(time<=18)
+        {
+            Console.WriteLine("iyi Günler");
+        }
+        else
+        {
+            Console.WriteLine("iyi geceler");
+        }
+        string sonuc = time<=18 ? "iyi günler": "iyi geceler";
+        sonuc=time>=6 && time<=11 ? "günaydın": time<=18 ?"iyi günler": "iyi geceler"; 
+        Console.WriteLine(sonuc);
+
+    }
+}
